cbf9b267147328584437def067c73defc585f403
dilshodovich001 <raufovmaruf777@gmail.com>
2026-02-17 20:20:52 +0500

    nimadir ozgarish

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,12 +2,13 @@ package com.example;
 
 public class Main {
     static void main() {
-        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+        for (int i = 0; i < Integer.MAX_VALUE / Short.MAX_VALUE; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
                 // assalom
             }
         }
+        System.out.println("aSDFGHJ,K");
 
         System.out.println("nima bunaqa narsa yozgansan ");
     }
